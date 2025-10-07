4cc0772d78cea598ade129bb96ad6061ee1a0f54
obidua <64002560+obidua@users.noreply.github.com>
2025-10-08 02:41:19 +0530

    Added COMPLETE_IMPLEMENTATION_SUMMARY.md

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from './auth.service';
 export * from './booking.service';
 export * from './vendor.service';
 export * from './therapist.service';
+export * from './admin.service';
+export * from './employee.service';
